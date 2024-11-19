e630f38b59487391f4ddb907743770acd10f4ea0
Phuc-2401 <hphuc2401@gmail.com>
2024-11-19 17:03:48 +0700

    update-toast

--- src/main.js
+++ src/main.js
@@ -2,12 +2,17 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import Vuetify from 'vuetify';
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 import 'vuetify/dist/vuetify.min.css';
 import '@mdi/font/css/materialdesignicons.css';
 
 Vue.config.productionTip = false;
 Vue.use(Vuetify);
-
+Vue.use(Toast, {
+  duration: 1500,
+  hideProgressBar: true,
+});
 new Vue({
   router,
   vuetify: new Vuetify(),
