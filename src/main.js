19ab289d7e268dd3958bf1094f95b80ed3fb3a64
ThanhTung-2010 <dtong201003@gmail.com>
2024-11-19 16:44:56 +0700

    login-page1

--- src/main.js
+++ src/main.js
@@ -2,17 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import Vuetify from 'vuetify';
-import Toast from 'vue-toastification';
-import 'vue-toastification/dist/index.css';
 import 'vuetify/dist/vuetify.min.css';
 import '@mdi/font/css/materialdesignicons.css';
 
 Vue.config.productionTip = false;
 Vue.use(Vuetify);
-Vue.use(Toast, {
-  duration: 3000,
-  hideProgressBar: true,
-});
+
 new Vue({
   router,
   vuetify: new Vuetify(),
