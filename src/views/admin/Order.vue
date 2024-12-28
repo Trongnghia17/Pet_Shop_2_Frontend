0b7dfdda633ee9d22b7e9ef789b7fb37202e5d10
Nghia <trannghia270403@gmail.com>
2024-12-28 17:32:25 +0700

    update đơn hàng quản lý

---
+++ src/views/admin/Order.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <div
+        style="font-size: 1.3rem; font-weight: 500; padding-bottom: 2rem  "
+    >
+      Danh sách đơn hàng
+    </div>
+    <v-row class="ma-0">
+
+    </v-row>
+
+
+  </div>
+</template>
+
+<script>
+
+import axiosInstance from "../../axiosInstance";
+
+
+export default {
+  name: 'CartPage',
+  components: {
+
+  },
+  data() {
+    return {
+
+      baseURL: axiosInstance.defaults.baseURL,
+    };
+  },
+  computed: {
+
+  },
+  watch: {},
+
+  mounted() {
+    this.getListOrder()
+
+  },
+  methods:{
+
+    getListOrder() {
+
+      apiConfig.getAllProduct(data).then((res) => {
+        console.log(res);
+
+        if (res.status === 200) {
+          this.listItemProduct = res.data.products;
+        }
+      });
+    },
+
+
+
+
+
+  }
+};
+</script>
+<style scoped>
+</style>
